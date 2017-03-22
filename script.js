075a0a0bc06d312c98e6cc6a3b3d3375ddd7a951
Marcel Jakob <marcel.jakob@hpe.com>
2017-03-22 10:00:35 +0100

    added create new data set

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-$( document ).ready(function() {
-			console.log("jquery works!")			
+var IPadress = 0
+var portNo = 0
+
+$( document ).ready(function(){
+	console.log("jquery works!")			
 });	
+
+// should work
+var newDataSet = function(data){	
+	$.ajax({
+    url: "http://"+ IPadress +":"+ portNo +"/buckets/example/keys",
+    type: "post",
+    headers: {
+        Content-Type: "text/plain"
+    },
+    dataType: 'json',
+    success: function (data) {
+        console.info(data);
+    }
+	});
+}
+
+// doesn't work
+var readAllDataSets = function(){
+	$.ajax({
+    url: "http://"+ IPadress +":"+ portNo +"/buckets/example/keys",
+    type: "get",
+    dataType: 'json',
+    success: function (data) {
+        console.info(data);
+    }
+	});
+}
+
+// nice to have
+var deletDataSet = function(){
+}
+
+// nice to have
+var modifyDataSet = function(){
+}
