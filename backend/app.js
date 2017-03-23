30ed846486773e4d608c3cf85dc842ad28fd094b
marcjako <jakob.marcel@web.de>
2017-03-23 20:27:21 +0100

    shit doesn't work

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var Riak = require('basho-riak-client');
+var assert = require('assert');
+
+var bucket = 'contributors'
 
 //construct express "application"
 var app = express();
 
 //connect to riak DB
+<<<<<<< HEAD
+var client = new Riak.Client(['127.0.0.1:8087'], function(err,c){
+	if(err){
+		console.log(err)
+		process.exit()
+	}
+});
+
+//check if node is available
+client.ping(function (err, rslt) {
+      if (err) {
+          console.log(err)
+	  process.exit()
+      } else {
+          // On success, ping returns true
+          assert(rslt === true);
+          console.log("connected to Riak node")
+      }
+=======
 var client = new Riak.Client(['127.0.0.1:8098'], function(err,c){
 	if(err){
 		console.log(err)
@@ -14,6 +36,7 @@ var client = new Riak.Client(['127.0.0.1:8098'], function(err,c){
 		//console.log(c.cluster)
 		console.log("connected to cluster")
 	}
+>>>>>>> 975facbd9823a7b916cbfb869a1cd736db2bd863
 });
 
 // parse application/json
@@ -21,14 +44,34 @@ app.use(bodyParser.json());
 
 // error handling
 app.use(function(err, req, res, next) {
+    console.log("POST error handling")
     console.error(err.stack);
-    res.status(500).send('failed');
+    res.status(500).send()
     next();
 });
 
 
-//define POST response for route "/students"
+//create a new data set
 app.post('/newDataSet', function (req, res, next) {
+<<<<<<< HEAD
+	console.log("POST /newDataSet")
+
+	var data = req.body   	
+	
+	client.storeValue({
+                bucket: bucket,
+                key: data.key,
+                value: data
+            },
+	function(err, rslt) {
+		if(err){
+					console.log("error: " + err)
+				}
+		}
+	);
+    console.log("inserted: " + JSON.stringify(data) + " into bucket " + bucket)			
+    res.status(201).send()
+=======
 	var data = req.body
     
 	var person = {
@@ -54,18 +97,47 @@ app.post('/newDataSet', function (req, res, next) {
 		
     res.writeHead(200, {"Content-Type": "text/html"});
     res.end("successfull");
+>>>>>>> 975facbd9823a7b916cbfb869a1cd736db2bd863
     next();
 });
 
-//define GET response for route "/students/:lastname"
-app.get('/getAllDataSets', function (req, res, next) {
-    res.send(students.filter(function (student) {
-        return student.lastName === req.params.lastname;
-    }));
-    next();
+//get all keys of a bucket
+app.get('/getAllKeys', function (req, res, next) {
+    console.log("GET /getAllKeys")
+	
+    client.listKeys({bucket: bucket},
+	    function (err, rslt) {
+		if (err) {
+		    console.log("error: " + err);
+		    res.status(404).send()			
+		} else {
+		    console.log(rslt)
+		}
+	    }
+	);
+     next();
 });
+
+//get one data set by key
+app.get('/getDataSet', function (req, res, next) {
+    console.log("GET /getDataSet")	
+   	
+    client.fetchValue({ bucket: bucket, key: '123', convertToJs: true},
+	    function (err, rslt) {
+		if (err) {
+		    console.log("error: " + err);
+		    res.status(404).send()			
+		} else {
+		    res.send(rslt.values.shift().value)
+		}
+	    }
+	);
+
+});
+
 
 //start express server (application) on port 3000 and log announce to console
 var server = app.listen(3000, function () {
     console.log('Express App listening at http://localhost:3000');
 });
+
